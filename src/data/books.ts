85ca7893c6f85f682560d3dcd0b9d45ed60b3c93
jvaler01 <jorgevm13@gmail.com>
2023-07-09 13:49:56 +0200

    transition api books example

---
+++ src/data/books.ts
@@ -0,0 +1,27 @@
+export const books = [
+    {
+        id: 'clean-code',
+        title: 'Clean Code: Manual de desarrollo agil de software',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg'
+    },
+    {
+        id: 'the-clean-coder',
+        title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg'
+    },
+    {
+        id: 'angular-projects',
+        title: 'Angular Projects: Build modern web apps in Angular 16 with 10 different projects and cutting-edge technologies',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1682670558i/135038384.jpg'
+    },
+    {
+        id: 'vue-fullstack',
+        title: 'Fullstack Vue: The Complete Guide to Vue.js',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1524731511i/39957496.jpg'
+    },
+    {
+        id: 'learning-angular',
+        title: 'Learning Angular: A no-nonsense guide to building web applications with Angular 15',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1681605848i/121453992.jpg'
+    }
+]
